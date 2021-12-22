0f0aa9b702f8217f29eee1890a9b958cf3a74ef3
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2021-12-22 16:21:53 -0300

    test: config factorybot syntax methods

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe Person, type: :model do
 
     context 'invalid cpf' do
       it 'is a invalid record' do
-        person = FactoryBot.build(:person, cpf: '1234455')
+        person = build(:person, cpf: '1234455')
         expect(person).not_to be_valid
       end
     end
 
     context 'invalid email' do
       it 'is a invalid record' do
-        person = FactoryBot.build(:person, email: 'not_an_email')
+        person = build(:person, email: 'not_an_email')
         expect(person).not_to be_valid
       end
     end
 
     context 'no email given' do
       it 'is a valid record' do
-        person = FactoryBot.build(:person, email: nil)
+        person = build(:person, email: nil)
         expect(person).to be_valid
       end
     end
