c45261e38ccdd043326065543e30c1a05b35d225
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2021-12-22 16:18:37 -0300

    test: validate cpf and email for person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,5 +4,26 @@ RSpec.describe Person, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:cpf) }
+
+    context 'invalid cpf' do
+      it 'is a invalid record' do
+        person = FactoryBot.build(:person, cpf: '1234455')
+        expect(person).not_to be_valid
+      end
+    end
+
+    context 'invalid email' do
+      it 'is a invalid record' do
+        person = FactoryBot.build(:person, email: 'not_an_email')
+        expect(person).not_to be_valid
+      end
+    end
+
+    context 'no email given' do
+      it 'is a valid record' do
+        person = FactoryBot.build(:person, email: nil)
+        expect(person).to be_valid
+      end
+    end
   end
 end
