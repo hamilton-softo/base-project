d69b6b6a985dc9469af851afbff9d5592e248942
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2021-12-22 18:30:14 -0300

    feat: add show action for person

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found(exception)
+    render json: { message: exception.message }, status: 404
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :people, only: :show, defaults: { format: :json }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'healthcheck' => 'healthcheck#index'
