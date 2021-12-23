8ae4eed39c82d7dc56d9acd2f3af059e428a3f52
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2021-12-23 09:53:13 -0300

    feat: add update action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,23 +1,36 @@
 class PeopleController < ApplicationController
+  before_action :set_person, only: %i[ show update ]
+
   def index
     @people = Person.all
   end
 
   def create
     @person = Person.create!(person_params)
+
     render 'people/show', status: 201
   end
 
+  def update
+    @person.update!(person_params)
+
+    render 'people/show', status: 200
+  end
+
   def show
-    @person = Person.find(params[:id])
   end
 
   def destroy
     Person.destroy(params[:id])
+
     head 204
   end
 
   private
+
+  def set_person
+    @person = Person.find(params[:id])
+  end
 
   def person_params
     params.require(:person).permit(:name, :cpf, :email, :birthday, :active)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :people, only: %i[ index create show destroy ], defaults: { format: :json }
+  resources :people, defaults: { format: :json }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'healthcheck' => 'healthcheck#index'
