7bd1dd7de1d1f28bfac8421bd3e7a5111d1ae272
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2021-12-23 15:02:07 -0300

    doc: add show action

--- spec/integration/people_spec.rb
+++ spec/integration/people_spec.rb
@@ -67,4 +67,47 @@ describe 'People API' do
       end
     end
   end
+
+  path '/people/{id}' do
+    get 'Retrieves a person' do
+      tags 'Person'
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string
+
+      response '200', 'person found' do
+        before { @person = create(:person) }
+
+        examples 'application/json': {
+          id: 1, name: 'James', email: 'some@nice.mail', address: 'Mulholland Drive', cpf: '419.356.777-05',
+          birthday: '2019-05-17', active: true
+        }
+
+        let(:id) { @person.id }
+
+        run_test! do |response|
+          expected = {
+            id: @person.id,
+            name: @person.name,
+            cpf: @person.cpf,
+            email: @person.email,
+            address: @person.address,
+            birthday: @person.birthday,
+            active: @person.active
+          }.to_json
+          expect(response.body).to eq(expected)
+        end
+      end
+
+      response '404', 'Not Found' do
+
+        examples 'application/json': { message: "Couldn't find Person with 'id'=0" }
+
+        let(:id) { 0 }
+
+        run_test! do |response|
+          expect(response.body).to eq({ message: "Couldn't find Person with 'id'=0" }.to_json)
+        end
+      end
+    end
+  end
 end
