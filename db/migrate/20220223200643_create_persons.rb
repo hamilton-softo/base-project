176879caae799cb5f414434c9bebcd3cfa582279
allanescossio <allanjohnson.pe@gmail.com>
2022-02-23 17:43:56 -0300

    Limita caracteres do CPF

--- db/migrate/20220223200643_create_persons.rb
+++ db/migrate/20220223200643_create_persons.rb
@@ -2,7 +2,7 @@ class CreatePersons < ActiveRecord::Migration[6.1]
   def change
     create_table :persons, id: :uuid do |t|
       t.string :name, null: false
-      t.string :cpf, null: false, unique: true
+      t.string :cpf, null: false, limit: 11, unique: true
       t.string :email, limit: 100, unique: true
       t.string :address
       t.date :birthday
